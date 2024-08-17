d80bfc43427fd5af5f06935f8ac91f6b135bc22a
inpathtamilan <inpathtamilan@onedatasoftware.com>
2024-08-17 18:45:39 +0530

    fields add

--- myappapp/types.py
+++ myappapp/types.py
@@ -8,6 +8,7 @@ class UserType(DjangoObjectType):
     class Meta:
         model = User
         filter_fields = ("username", "email")
+        # fields = ("id", "username", "email")
         interfaces = (relay.Node,)
         
         
@@ -15,10 +16,12 @@ class AuthorType(DjangoObjectType):
     class Meta:
         model = Author
         filter_fields = ("birth_date", "email")
+        fields = ("id", "first_name", "last_name", "birth_date", "email")
         interfaces = (relay.Node,)
 
 class BookType(DjangoObjectType):
     class Meta:
         model = Book
         filter_fields = ("title",)
+        fields = ("id", "title", "publication_date", "isbn", "pages", "cover", "language", "book_image", "author")
         interfaces = (relay.Node,)
